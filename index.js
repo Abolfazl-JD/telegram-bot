401ec287a749c2f19c1153cf6d01c717f6caec30
Abolfazl jalildoost <abolfazljalildoost@gmail.com>
2024-02-12 18:46:38 +0330

    remove tel token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
 
-const token = '6411429669:AAHmxnJXuJ2Vm92rDvdGstXKeKkE4UrpGP0'
+const token = 'token'
 
 const bot = new TelegramBot(token, { polling: true })
 
